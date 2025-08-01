c20c271ae0b0980f48e798f48d440e9bf47792ef
adekunle.adejokun <adekunle.adejokun@gmail.com>
2025-08-01 22:30:53 +0000

    chore: update CI workflow and Jest config for improved coverage reporting
    Co-authored-by: Genie <genie@cosine.sh>

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,19 @@ module.exports = {
   // Ignore pino-pretty noise in tests
   silent: true,
   // Add custom Jest config as needed
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "server/**/*.js",
+    "!server/common/tracing.js",
+    "!**/node_modules/**",
+    "!**/tests/**",
+  ],
 };
