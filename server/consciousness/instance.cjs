c37ed6189a79b0c2d7ee7fcf953c488a4a8a95cf
Ulysses <65002977+kunleulysses@users.noreply.github.com>
2025-08-06 00:25:16 -0400

    feat: add scene worker for reality generation

--- server/consciousness/instance.cjs
+++ server/consciousness/instance.cjs
@@ -2,6 +2,7 @@ import { HolographicConsciousnessRealityGenerator } from './holographic-consciou
 import './eventHandlers/realityQueueProducer.cjs';
 import './metricsServer.cjs';
 import './workers/placement.js';
+import './workers/sceneWorker.js';
 
 const generator = new HolographicConsciousnessRealityGenerator();
 export default generator;
