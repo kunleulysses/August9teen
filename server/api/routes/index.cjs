16a7d53a945a22866f7a5468681006a1348b0570
Ulysses <65002977+kunleulysses@users.noreply.github.com>
2025-08-05 23:14:41 -0400

    feat: add memory placement worker

--- server/api/routes/index.cjs
+++ server/api/routes/index.cjs
@@ -1,5 +1,7 @@
 const express = require('express');
 const realities = require('./realities.cjs');
+const memory = require('./memory.cjs');
 const router = express.Router();
 router.use('/realities', realities);
+router.use('/memory', memory);
 module.exports = router;
--- server/consciousness/instance.cjs
+++ server/consciousness/instance.cjs
@@ -1,6 +1,7 @@
 import { HolographicConsciousnessRealityGenerator } from './holographic-consciousness-reality-generator.cjs';
 import './eventHandlers/realityQueueProducer.cjs';
 import './metricsServer.cjs';
+import './workers/placement.js';
 
 const generator = new HolographicConsciousnessRealityGenerator();
 export default generator;
