98af027e3ac5b4926d4a8fecf6b2f18ae684efe0
Ulysses <65002977+kunleulysses@users.noreply.github.com>
2025-08-06 00:34:33 -0400

    refactor: update tests to use server modules

--- __tests__/topology/topology.spec.ts
+++
@@ -1,7 +0,0 @@
-import { propertyTestReversibility } from '../../FlappyJournal/server/consciousness/core/math/topologyProof';
-
-describe('HyperdimensionalSpiralTopology property tests', () => {
-  it('reversible foldTraversal', () => {
-    propertyTestReversibility(7, 50); // throws if fails
-  });
-});
